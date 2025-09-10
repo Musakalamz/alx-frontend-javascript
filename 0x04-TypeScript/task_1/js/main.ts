d8e37adcf2f8b63e6bf1f9946b7fbd1c9bfb1a76
Musakalamz <ogunsolumusa@gmail.com>
2025-09-10 11:47:21 +0100

    Function rewritten

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,14 +11,16 @@ interface Director extends Teacher {
   numberOfReports: number;
 }
 
-
-
 interface PrintTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-
-function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
-  return `${firstName[0]}. ${lastName}`;
+function printTeacher({
+  firstName,
+  lastName,
+}: {
+  firstName: string;
+  lastName: string;
+}): string {
+  return `${firstName}. ${lastName}`;
 }
-
