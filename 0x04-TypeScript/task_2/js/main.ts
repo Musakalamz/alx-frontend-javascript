ad7b3cf05258ac7272f18569aa350556d7a79459
Musakalamz <ogunsolumusa@gmail.com>
2025-09-11 23:43:27 +0100

    String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -45,13 +45,24 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
-function isDirector(employee: Director | Teacher) {
+export function isDirector(employee: Director | Teacher) {
   return employee instanceof Director;
 }
 
-function executeWork(employee: Director | Teacher) {
+export function executeWork(employee: Director | Teacher) {
   if (isDirector(employee)) {
-    return (employee as Director).workDirectorTasks();
+    return employee.workDirectorTasks();
   }
-  return (employee as Teacher).workTeacherTasks();
+  return employee.workTeacherTasks();
+}
+
+export type Subjects = "Math" | "History";
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  }
+  if (todayClass === "History") {
+    return "Teaching History";
+  }
 }
