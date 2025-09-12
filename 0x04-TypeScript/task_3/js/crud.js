1b365d42f551a0497d4072bf8475863b92d74512
Musakalamz <ogunsolumusa@gmail.com>
2025-09-12 09:23:11 +0100

    Ambient Namespaces

--- 0x04-TypeScript/task_3/js/crud.js
+++ 0x04-TypeScript/task_3/js/crud.js
@@ -1,5 +1,15 @@
-import { RowID, RowElement } from "./interface";
+export function insertRow(row) {
+  console.log("Insert row", row);
+  return Math.floor(Math.random() * Math.floor(1000));
+}
 
-declare function insertRow(row: RowElement): number;
-declare function deleteRow(rowId: RowID): void;
-declare function updateRow(rowId: RowID, row: RowElement): number;
+export function deleteRow(rowId) {
+  console.log("Delete row id", rowId);
+  return;
+}
+
+export function updateRow(rowId, row) {
+  console.log(`Update row ${rowId}`, row);
+
+  return rowId;
+}
